00c46d4913d326a5546504d331a53c5c22351122
CT-Vishwas <vishwas@cloudthat.com>
2025-02-28 15:53:05 +0530

    Add files via upload

---
+++ libraryManagementSystem/src/main/java/com/cloudthat/librarymanagement/Book.java
@@ -0,0 +1,65 @@
+package com.cloudthat.librarymanagement;
+
+import java.util.Objects;
+
+public class Book {
+    // access modifiers : public, protected, default, private
+    // private: only access through the class
+    private String isbn;
+    private String title;
+    private String author;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn, title, author);
+    }
+}
